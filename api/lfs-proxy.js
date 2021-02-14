45e5603dc784227e18479be7d9ef1735b9a00779
Jacob Ford <j@cobford.com>
2021-02-14 11:57:17 -0500

    Create proxy from /api/lfs-proxy to GitHub LFS server

---
+++ api/lfs-proxy.js
@@ -0,0 +1,17 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const lfsProxy = createProxyMiddleware({
+  target: 'https://media.githubusercontent.com/media/unitof/vercel-git-lfs-test/main/',
+  pathRewrite: {
+    '^/api/lfs-proxy': ''
+  }
+})
+
+module.exports = (req, res) => {
+  lfsProxy(req, res, (result) => {
+    if (result instanceof Error) {
+      throw result
+    }
+    throw new Error(`Proxy error with url: '${req.url}'`);
+  })
+}
